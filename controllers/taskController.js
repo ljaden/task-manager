29d478ba45e6e6a940d03d570372310265db6032
jaden <ljaden.9@gmail.com>
2022-08-29 17:37:19 -0400

    fixed PATCH route functionality

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -64,13 +64,23 @@ const updateTask = async (req, res) => {
     const taskID = req.params.id;
     const updatedTask = req.body;
 
-    const results = await db.findByIdAndUpdate({ _id: taskID }, updatedTask, {
+    const task = await db.findByIdAndUpdate({ _id: taskID }, updatedTask, {
       returnDocument: "after",
     });
 
+    if (!task) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: `No task found with id: ${taskID}`,
+      });
+    }
+
     res.status(200).json({
       statusCode: 200,
-      message: updatedTask,
+      message: {
+        update: updatedTask,
+        result: task,
+      },
     });
   } catch (error) {
     res.status(500).json({
